b38b27a8c317fb4e73f571ca0509f5a39986f792
lemonmaru <uryohei4@gmail.com>
2022-06-29 22:17:29 +0900

    add basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    assetPrefix: process.env.NODE_ENV === "production" ? "/blog" : "",
+    basePath: process.env.NODE_ENV == "production" ? "/blog" : "",
+
+    assetPrefix: process.env.NODE_ENV == "production" ? "/blog" : "",
 
     publicRuntimeConfig: {
-        basePath: process.env.NODE_ENV === "production" ? "/blog" : "",
+        basePath: process.env.NODE_ENV == "production" ? "/blog" : "",
     },
 };
