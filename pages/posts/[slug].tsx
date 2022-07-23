860f02846c4b84979e06e6e88b3b2c63c47dc468
lemonmaru <uryohei4@gmail.com>
2022-07-23 14:40:59 +0900

    apply zenn css

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -9,7 +9,9 @@ import { getPostBySlug, getAllPosts } from '../../lib/api'
 import PostTitle from '../../components/post-title'
 import Head from 'next/head'
 import { CMS_NAME } from '../../lib/constants'
-import markdownToHtml from '../../lib/markdownToHtml'
+//import markdownToHtml from '../../lib/markdownToHtml'
+import 'zenn-content-css';
+import markdownToHtml from 'zenn-markdown-html';
 import PostType from '../../types/post'
 
 type Props = {
@@ -31,7 +33,7 @@ const Post = ({ post, morePosts, preview }: Props) => {
           <PostTitle>Loading…</PostTitle>
         ) : (
           <>
-            <article className="mb-32">
+            <article className="mb-32 znc">
               <Head>
                 <title>
                   {post.title} | Next.js Blog Example with {CMS_NAME}
