26ef5dde7add65e32db0bf448fada7b1c9931577
lemonmaru <uryohei4@gmail.com>
2022-07-24 19:57:12 +0900

    fix sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://u100golf.github.io/blog',
+    siteUrl: 'https://u100golf.github.io/blog',
     generateRobotsTxt: true, // (optional)
     outDir: './docs'
         // ...other options
